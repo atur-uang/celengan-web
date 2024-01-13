c264598de9fe4849535cb1388e6229a453b6067f
Fathur Rohman <hi.fathur.rohman@gmail.com>
2024-01-13 18:10:16 +0700

    use next navigation

--- src/app/vehicles/create/page.tsx
+++ src/app/vehicles/create/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import {useState, FormEvent} from 'react'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 import {axios} from '@/helpers';
 
